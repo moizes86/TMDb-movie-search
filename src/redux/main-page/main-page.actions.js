c3001afa821a0bafb20d7bd2c60cb11165f6a99a
moizes86 <moshe.mn86@gmail.com>
2020-09-28 22:26:52 +0300

    trying to hide api key on netlify

--- src/redux/main-page/main-page.actions.js
+++ src/redux/main-page/main-page.actions.js
@@ -1,7 +1,7 @@
 import MainPageTypes from './main-page.types';
 import { formatData } from './main-page.utils';
 
-const API_KEY = process.env.API_KEY;
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 export const onFetchMovieAsync = (movieID) => {
   return dispatch => {
